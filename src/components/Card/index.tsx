edcfde5a0104dc031c22ab0bbe1ea6c6535f6274
LaislaSte <stephenielaisla@gmail.com>
2022-11-13 19:00:10 -0300

    refact: added card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Button from '../Button';
+import Input from '../Input';
 
-const Card = () => {
+const Card = (prop: { title: string; }) => {
   return (
-    <div>Card</div>
+    <div className='flex flex-col items-center justify-center border border-sky-900 gap-4 w-1/2 p-4	bg-slate-800 shadow-md rounded-md py-2 px-3 select-none cursor-pointer transition-all hover:bg-blue hover:text-black '>
+      <div id="header">
+        <h1 className='text-sky-500 font-semibold uppercase'> {prop.title} </h1>
+
+      </div>
+
+      <form action="" id="form" className='flex flex-col items-center justify-center gap-8 w-full '>
+        <Input placeholder='Write a text' />
+        <Input placeholder='Write a text' />
+
+        <Button placeholder='Send' type='submit' />
+      </form>
+
+    </div>
   )
 }
 
