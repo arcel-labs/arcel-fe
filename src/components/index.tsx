b279069dd5000a1fdf803495c633db563befd448
ArielBetti <ariel.betti@gmail.com>
2022-11-13 20:20:59 -0300

    fix: components imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import Card from './Card';
 import Button from './Button';
+import Card from './Card';
 import Input from './Input';
+import UserProfile from './Atoms/UserProfile';
 
-export { Card, Button, Input };
-=======
-import Card from "./Card";
-import Button from "./Button";
-import UserProfile from "./Atoms/UserProfile";
-
-export { Card, Button, UserProfile };
->>>>>>> 15f7c17540243bc996a5737d87d637c556b2e174
+export { Button, Card, Input, UserProfile };
