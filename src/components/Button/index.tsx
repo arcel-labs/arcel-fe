b216731d8e075ebcfed1f176bf245ffc7142b6d9
LaislaSte <stephenielaisla@gmail.com>
2022-11-13 18:57:32 -0300

    chore: modified button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,24 +1,24 @@
 import { ButtonHTMLAttributes, FC } from "react";
 
 export interface ButtonProps
-extends ButtonHTMLAttributes<HTMLButtonElement> {
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   placeholder?: string,
 }
 
 const Button: FC<ButtonProps> = ({
-    placeholder,
-    type,
-    onClick
+  placeholder,
+  type,
+  onClick
 }) => {
   return (
     <>
-    <button
-        className="bg-violet-600 hover:bg-violet-700 tran p-2 rounded-3xl w-32 dtransition delay-100 duration-200 ease-in-out"
+      <button
+        className="bg-sky-600 hover:bg-sky-700 hover:text-white tran p-2 rounded-3xl w-32 dtransition delay-100 duration-200 ease-in-out"
         type={type}
         onClick={onClick}
-    >
+      >
         {placeholder}
-    </button>
+      </button>
     </>
   )
 }
