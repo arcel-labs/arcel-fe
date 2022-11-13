8688e8ec460319c6365e46d3ecdf7aa36b4af1ac
LaislaSte <stephenielaisla@gmail.com>
2022-11-13 20:53:26 -0300

    refact: created loader component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ import Button from './Button';
 import Card from './Card';
 import Input from './Input';
 import UserProfile from './Atoms/UserProfile';
+import Loader from './Loader';
 
-export { Button, Card, Input, UserProfile };
+export { Button, Card, Input, UserProfile, Loader };
